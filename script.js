c5017930c387304ade50878a8f35dde641438350
Victor Oliveira <oliveira.vitoco@gmail.com>
2020-11-10 15:22:04 -0300

    Melhorias na lógica do código

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function converter(){
-	var ad = document.getElementById("userinput").value;
-	let number = parseInt(ad, 2)
-    tela(number)
-
-}
+var tel = document.getElementById("output");
+var ad =document.getElementById("userinput")
 
 function limpartela(){
 	document.getElementById("userinput").value = ''
@@ -13,5 +9,19 @@ function limpartela(){
 function tela(value){
 	tel.value = value
 }
+ function converter(){
+ 	var adValue = reverseString(ad.value) 
+ 	var i;
+ 	var outrovalor = 0;
+ 	for(i = 0; i < adValue.length; i++){
+ 		outrovalor += Number(adValue[i]) * Math.pow(2, i) 
+ 	}
+ 	tela(outrovalor)
+ }
 
-var tel = document.getElementById("output");
+function reverseString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
+}
